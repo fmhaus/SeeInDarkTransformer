482b1c2c8fdc2f3524a42cd83784666c4275c629
fmhaus <florianhaus1703@gmail.com>
2025-09-16 15:09:16 +0200

    update dataset file

--- download_dataset_sony.sh
+++ download_dataset_sony.sh
@@ -1,4 +1,4 @@
-wget "https://storage.googleapis.com/isl-datasets/SID/Sony.zip"
+wget "https://storage.googleapis.com/isl-datasets/SID/Sony2025.zip"
 mkdir dataset
 unzip Sony2025.zip -d dataset
 rm Sony2025.zip
