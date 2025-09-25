d852a5b0c77f845e49140fe871700c9f979e3274
fmhaus <florianhaus1703@gmail.com>
2025-09-25 20:43:55 +0200

    fix optimizer loading

--- train_model.py
+++ train_model.py
@@ -150,7 +150,7 @@ if __name__ == '__main__':
             lr_schedule_first_epoch = log['lr_schedule_first_epoch']
            
             optimizer_file = os.path.join(opt.out_folder, f'optimizer_checkpoint_{opt.resume_epoch}.pt')
-            optimizer_checkpoint = torch.load(optimizer_file, weights_only=True, map_location=device)
+            optimizer_checkpoint = torch.load(optimizer_file, weights_only=True)
                 
             optimizer.load_state_dict(optimizer_checkpoint)
             print(f'Loaded optimizer_checkpoint_{opt.resume_epoch}.pt.')
